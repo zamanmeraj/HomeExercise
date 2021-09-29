6fe427c88ad5553c5fbe33d665f4cf4e0d8b84b4
Zaman Meraj <merajzaman865@gmail.com>
2021-09-29 23:05:37 +0530

    Task1#Project setup

---
+++ HomeExercise/HomeExercise/MVVM/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeExercise
+//
+//  Created by Zaman Meraj on 29/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
